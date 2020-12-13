3c0930686f3807eb1ff46b77b250f51ca432ff95
JulSav <57712373+JulSav@users.noreply.github.com>
2020-12-14 00:33:37 +0200

    Add files via upload

--- src/main/java/Serv/Servlet1.java
+++ src/main/java/Serv/Servlet1.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 @WebServlet("/hello")
+
 public class Servlet1 extends HttpServlet {
 
     @Override
